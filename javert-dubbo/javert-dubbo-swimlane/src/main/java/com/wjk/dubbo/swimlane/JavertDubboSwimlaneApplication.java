e00d681ff897da200f518699bb58a496e02f26bc
junkai.wang <junkai.wang@xinjifamily.com>
2023-05-09 17:22:46 +0800

    新增dubbo-swimlane模块

---
+++ javert-dubbo/javert-dubbo-swimlane/src/main/java/com/wjk/dubbo/swimlane/JavertDubboSwimlaneApplication.java
@@ -0,0 +1,13 @@
+package com.wjk.dubbo.swimlane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavertDubboSwimlaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavertDubboSwimlaneApplication.class, args);
+    }
+
+}
---
+++ javert-dubbo/javert-dubbo-swimlane/src/test/java/com/wjk/dubbo/swimlane/JavertDubboSwimlaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjk.dubbo.swimlane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavertDubboSwimlaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
