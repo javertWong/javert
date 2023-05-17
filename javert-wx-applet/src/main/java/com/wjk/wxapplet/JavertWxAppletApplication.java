e14aab803b5940bdf44602d2f2d40aeb8682be21
junkai.wang <junkai.wang@xinjifamily.com>
2023-05-17 19:17:38 +0800

    新增dubbo-swimlane-provider模块

---
+++ javert-wx-applet/src/main/java/com/wjk/wxapplet/JavertWxAppletApplication.java
@@ -0,0 +1,13 @@
+package com.wjk.wxapplet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavertWxAppletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavertWxAppletApplication.class, args);
+    }
+
+}
---
+++ javert-wx-applet/src/test/java/com/wjk/wxapplet/JavertWxAppletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjk.wxapplet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavertWxAppletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
